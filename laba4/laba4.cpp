5b285068987f25f40725bf57327a5a8dc6555953
DANYA_ <danya.nosyk@gmail.com>
2019-10-18 14:53:22 +0300

    laba4

--- laba4/laba4.cpp
+++ laba4/laba4.cpp
@@ -2,9 +2,45 @@
 //
 
 #include <iostream>
+#include<math.h>
+using namespace std;
+const int n = 4;
 
+int mas[n][n];
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+	
+
+	for (int i = 0; i < n;i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> *(*(mas + i) + j);				
+		}
+		cout << "\n end line"<<endl;
+	}
+
+	for (int j = 0; j < n;j++) {
+		for (int i = 0; i < n; i++) {
+			cout << "\n" << i << " " << j;
+
+			if (*(*(mas + i) + j)==0) {
+				break;
+			}
+		}
+	}
+	
+
+	
+}
+int get_sum(int num_str) {
+	int sum = 0;
+	for (int i = 0;i < n;i++) {
+		int* ptrtemp = &*((*mas + num_str) + i);
+			if ((abs(*ptrtemp) == *ptrtemp)&& ( *ptrtemp % 2 == 0)) {
+				sum += *ptrtemp;
+			
+			}
+	}
+	return sum;
 }
 
