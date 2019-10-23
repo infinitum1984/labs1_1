106139998fbe19dc42a2c285e7e30e8315cd2143
infinitum1984 <danya.nosyk@gmail.com>
2019-10-23 08:40:34 +0300

    laba 1 fix

--- laba1/laba1.cpp
+++ laba1/laba1.cpp
@@ -1,34 +1,20 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-<<<<<<< HEAD
 
 	int main()
 	{
-		double m, c, x, b;
-		
-=======
-	int main()
-	{
-		double m, c, x, b;
->>>>>>> ebc5bbc15fc1bce68f8e2d0e6c7c7d4b031ea16a
-		cout << "m: ";
-		cin >> m;
+		double m=2;
 
-		cout << "c: ";
-		cin >> c;
-
-		cout << "x: ";
-		cin >> x;
-
-		cout << "b: ";
-		cin >> b;
-
+		double c=-1;
+		double x=1.2;
+		double b=0.7;
+				
 		double fi = sqrt(m * tan(x) + abs(c * sin(x)));
 		double eps = m * cos(b * x + sin(x)) + (c * c * c);
 
-		cout << "fi: " << fi << "\n";
-		cout << "eps: " << eps << "\n";
+		cout << "A: " << fi << "\n";
+		cout << "K: " << eps << "\n";
 		return 0;
 	}
 
