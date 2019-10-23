71e1fb58b77cc8a13a2fe4f325b81ced6ba5adc4
infinitum1984 <danya.nosyk@gmail.com>
2019-10-23 08:42:30 +0300

    laba 1 fix

--- laba1/laba1.cpp
+++ laba1/laba1.cpp
@@ -5,16 +5,15 @@ using namespace std;
 	int main()
 	{
 		double m=2;
-
 		double c=-1;
 		double x=1.2;
 		double b=0.7;
 				
-		double fi = sqrt(m * tan(x) + abs(c * sin(x)));
-		double eps = m * cos(b * x + sin(x)) + (c * c * c);
+		double A = sqrt(m * tan(x) + abs(c * sin(x)));
+		double K = m * cos(b * x + sin(x)) + (c * c * c);
 
-		cout << "A: " << fi << "\n";
-		cout << "K: " << eps << "\n";
+		cout << "A: " << A << "\n";
+		cout << "K: " << K << "\n";
 		return 0;
 	}
 
